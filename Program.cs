77a84b1ad3cf98a4e4d7a53bb7d8cef4270066b6
Ibrahim <mamedovarbeiter@gmail.com>
2022-11-19 16:29:04 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type numbers");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
