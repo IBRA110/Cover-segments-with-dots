a9a85b900b7fcc165b9da95bcf2e09600c5349f0
Ibrahim <mamedovarbeiter@gmail.com>
2022-11-23 01:09:51 +0300

    created sagnment struct

--- Program.cs
+++ Program.cs
@@ -1,16 +1,51 @@
-﻿namespace Program
+﻿
+
+namespace Program
 {
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Type numbers");
+        {            
             int n = Convert.ToInt32(Console.ReadLine());
             
-            for (int i = 0; i < n; i++) 
+            Segment[] arr = new Segment[n];
+            
+            for (int i = 0; i < arr.GetLength(0); i++)
             {
-                Console.WriteLine(i);
+                arr[i] = new Segment(Console.ReadLine().Split());
+            }
+            
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.Write(arr[i].left.ToString() + " ");
+                Console.Write(arr[i].right.ToString() + " ");
+                Console.WriteLine();
             }
         }
+        struct Segment: IComparable<Segment>
+        {
+            public int left;
+            public int right;
+
+            public Segment(string[] str)
+            {
+                left = int.Parse(str[0]);
+                right = int.Parse(str[1]);
+            }
+
+            public int CompareTo(Segment other)
+            {
+                return right.CompareTo(other.right);
+            }
+
+            public override string ToString()
+            {
+                return $"[{left}, {right}]";
+            }
+        }      
     }
 }
